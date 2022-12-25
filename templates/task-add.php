7cc5564b0cdcf498775ab2f7f95b51f44434d0f1
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-25 14:30:31 +1000

    Add files via upload

---
+++ templates/task-add.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var string input_name */
+/** @var array errors */
+
+?>
+
+<div class="registration-block">
+    <form method="post" action="">
+        <h2 class="text-center">TASK</h3>
+
+        <div class="form-group">
+            <input class="form-control item"
+                type="text"
+                name="<?= $input_name ?>"
+                id="username"
+                value="<?= $_POST[$input_name] ?? '' ?>"
+                placeholder="Name"
+            >
+
+            <?php if (isset($errors[$input_name])): ?>
+                <p style="color: red; font-weight: bold; text-align: center; margin: -20px 0 20px 0;"><?= $errors[$input_name] ?></p>
+            <?php endif; ?>
+
+        </div>
+
+        <div class="form-group">
+            <button
+                class="btn btn-primary btn-block log-in"
+                type="submit"
+            >Add</button>
+        </div>
+
+    </form>
+</div>
