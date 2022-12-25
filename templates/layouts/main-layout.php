498b7c6db4b204fbd4215ebda1ce3535193301fc
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2022-12-25 14:33:47 +1000

    Add files via upload

---
+++ templates/layouts/main-layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title><?= $title ?></title>
+        <link rel="stylesheet" href="<?= $css_file ?>">
+        <link rel="shortcut icon" href="pictures/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+
+        <?php if (isset($page_header)): ?>
+            <?= $page_header ?>
+        <?php endif; ?>
+
+        <?= $page_content ?>
+    </body>
+</html>
