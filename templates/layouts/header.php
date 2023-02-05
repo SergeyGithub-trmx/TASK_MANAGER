32d2fdbbad4eb5305ebd3a36300868bc912697f1
SergeyGithub-trmx <65391743+SergeyGithub-trmx@users.noreply.github.com>
2023-02-05 13:31:13 +1000

    Add files via upload

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,10 +1,11 @@
-<header>
+<header class="page-header">
     <?php if (isset($_COOKIE['register-successful'])): ?>
         <?php setcookie('register-successful', 1, time() - 3600); ?>
         Register completed successfully!
     <?php else: ?>
-        Universal Task Manager
+        <h2>Universal Task Manager</h2>
         <?php if (isset($_SESSION['user'])): ?>
+            <p style="order: -1; font-family: Aller; color: #3fcf3f"><?= esc($_SESSION['user']['login']) ?></p>
             <a class="logout" href="./logout.php">Log out</a>
         <?php endif; ?>
     <?php endif; ?>
